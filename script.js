7453dcd5e8e0d3ec12f66dc053c0a3d9d662b1c3
livemer <savosteevmark@gmail.com>
2025-03-09 18:43:24 +0300

    Commit

---
+++ script.js
@@ -0,0 +1,79 @@
+/** @format */
+
+let items = document.querySelectorAll(".item")
+let next_player = document.querySelector(".next__player")
+const u = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+let isPlayer1 = true
+let play = true
+
+for (let i = 0; i < items.length; i++) {
+  items[i].id = i
+}
+
+items.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    if(!play) return
+    if (element.innerHTML.trim() == "") {
+      if (isPlayer1) {
+        element.innerText = "X"
+        next_player.innerText = "O"
+        isPlayer1 = false
+      } else {
+        element.innerText = "O"
+        next_player.innerText = "X"
+        isPlayer1 = true
+      }
+    }
+    WinCheck()
+  })
+})
+
+document.querySelector(".restart").addEventListener("click", function (e) {
+  items.forEach((element) => {
+    element.innerText = ""
+  })
+  isPlayer1 = true
+  next_player.innerText = "X"
+})
+
+function WinCheck() {
+  player1Arr = []
+  player2Arr = []
+  items.forEach((element) => {
+    if (element.innerText == "X") {
+      player1Arr.push(+element.id)
+    } else if (element.innerText == "O") {
+      player2Arr.push(+element.id)
+    }
+  })
+  for (let i = 0; i < u.length; i++) {
+    if (player1Arr.includes(u[i][0]) &&player1Arr.includes(u[i][1]) &&player1Arr.includes(u[i][2])) {
+      alert("Победил X")
+      document.querySelector("h2").innerText = "Победил X"
+      OnEnd()
+    }
+    else if (player2Arr.includes(u[i][0]) &&player2Arr.includes(u[i][1]) &&player2Arr.includes(u[i][2])) {
+      alert("Победил O")
+      document.querySelector("h2").innerText = "Победил O"
+      OnEnd()
+    }
+  }
+}
+
+function OnEnd(){
+  next_player.innerText = ""
+  play = false
+  items.forEach((element) => {
+    element.style.color = "gray"
+    element.style.bordercolor = "gray"
+  })
+}
